e4af4edd853eefb72502cedb890ac1065b44b53f
Scott <scottalancooper@gmail.com>
2015-04-30 23:47:28 +0100

    Updates Libraries
    Also added Crashlytics

--- app/src/main/java/com/eden/youtubesample/MainActivity.java
+++ app/src/main/java/com/eden/youtubesample/MainActivity.java
@@ -1,18 +1,22 @@
 package com.eden.youtubesample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.youtube.player.YouTubeApiServiceUtil;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 
+import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
