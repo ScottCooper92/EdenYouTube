3d6e85ab165a57b6b7313c94d83f4b8ce37eecea
Scott <scottalancooper@gmail.com>
2015-07-21 11:58:01 +0100

    Attempting to mitigate the issue caused by #5 and also cleaned up the getView method to avoid unnecessary code and thumbnail flickering

--- app/src/main/java/com/eden/youtubesample/VideoListAdapter.java
+++ app/src/main/java/com/eden/youtubesample/VideoListAdapter.java
@@ -18,19 +18,19 @@ import java.util.Map;
 
 /***********************************************************************************
  * The MIT License (MIT)
-
+ * <p/>
  * Copyright (c) 2015 Scott Cooper
-
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
-
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -68,51 +68,63 @@ public class VideoListAdapter extends BaseAdapter implements YouTubeThumbnailVie
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View currentRow = convertView;
         VideoHolder holder;
 
         //The item at the current position
         final YouTubeContent.YouTubeVideo item = YouTubeContent.ITEMS.get(position);
 
-        if (currentRow == null) {
+        if (convertView == null) {
             //Create the row
             final LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            currentRow = inflater.inflate(R.layout.row_layout, parent, false);
+            convertView = inflater.inflate(R.layout.row_layout, parent, false);
 
             //Create the video holder
             holder = new VideoHolder();
 
             //Set the title
-            holder.title = (TextView) currentRow.findViewById(R.id.textView_title);
+            holder.title = (TextView) convertView.findViewById(R.id.textView_title);
             holder.title.setText(item.title);
 
             //Initialise the thumbnail
-            holder.thumb = (YouTubeThumbnailView) currentRow.findViewById(R.id.imageView_thumbnail);
+            holder.thumb = (YouTubeThumbnailView) convertView.findViewById(R.id.imageView_thumbnail);
             holder.thumb.setTag(item.id);
             holder.thumb.initialize(mContext.getString(R.string.DEVELOPER_KEY), this);
 
-            currentRow.setTag(holder);
+            convertView.setTag(holder);
         } else {
             //Create it again
-            holder = (VideoHolder) currentRow.getTag();
+            holder = (VideoHolder) convertView.getTag();
             final YouTubeThumbnailLoader loader = mLoaders.get(holder.thumb);
 
-            //Set the title
             if (item != null) {
+                //Set the title
                 holder.title.setText(item.title);
+
+                //Setting the video id can take a while to actually change the image
+                //in the meantime the old image is shown.
+                //Removing the image will cause the background color to show instead, not ideal
+                //but preferable to flickering images.
+                holder.thumb.setImageBitmap(null);
 
                 if (loader == null) {
                     //Loader is currently initialising
                     holder.thumb.setTag(item.id);
                 } else {
                     //The loader is already initialised
-                    loader.setVideo(item.id);
+                    //Note that it's possible to get a DeadObjectException here
+                    try {
+                        loader.setVideo(item.id);
+                    } catch (IllegalStateException exception) {
+                        //If the Loader has been released then remove it from the map and re-init
+                        mLoaders.remove(holder.thumb);
+                        holder.thumb.initialize(mContext.getString(R.string.DEVELOPER_KEY), this);
+
+                    }
                 }
 
             }
         }
-        return currentRow;
-
+        return convertView;
     }
 
 
