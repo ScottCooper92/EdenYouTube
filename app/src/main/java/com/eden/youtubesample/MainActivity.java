c8590526bd00d190ff337e3678c184c0832150c4
Scott <scottalancooper@gmail.com>
2016-01-25 10:56:10 +0000

    Removed toolbar from version 11 and below to prevent crashes

--- app/src/main/java/com/eden/youtubesample/MainActivity.java
+++ app/src/main/java/com/eden/youtubesample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.eden.youtubesample;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,19 +10,19 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 
 /***********************************************************************************
  * The MIT License (MIT)
-
+ * <p/>
  * Copyright (c) 2015 Scott Cooper
-
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ * <p/>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
-
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,8 +39,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Only add the toolbar if we are on Honeycomb and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+        }
 
 
         //Check for any issues
